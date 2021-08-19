d14e665244a4d3b4f4664cc2df1cd37539bdd295
wiliam <wiliam@blake.ares>
2021-08-19 20:21:01 +0430

    new branch

---
+++ src/components/PureTaskList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  import Task from './Task';
+  export default {
+    name: 'PureTaskList',
+    // same content as before
+  };
+</script>
